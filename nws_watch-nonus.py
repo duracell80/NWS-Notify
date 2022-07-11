d4231f167ab845828baa14f04f5f84076a81aba8
Lee Jordan <duracell80@users.noreply.github.com>
2022-07-10 21:13:23 -0500

    NonUS enhancements

--- nws_watch-nonus.py
+++ nws_watch-nonus.py
@@ -18,12 +18,11 @@ cfg_urlsht          = cfg.get('nws_conf', 'urlsht', fallback='off')
 
 cfg_region_nonus    = cfg.get('nws_conf-nonus', 'region', fallback='PR')
 cfg_locale_nonus    = cfg.get('nws_conf-nonus', 'locale', fallback='Guarda,Vila Real')
-cfg_locales_nonus   = cfg_locale_nonus.split(",")
+cfg_locales_nonus   = cfg_locale_nonus.lower().split(",")
 cfg_feedid_nonus    = cfg.get('nws_conf-nonus', 'feedid', fallback='pt-ipma-pt')
 feed_nonus          = f"http://alert-feed.worldweather.org/{cfg_feedid_nonus.lower()}/rss.xml"
-
 cfg_kwords_nonus    = cfg.get('nws_conf-nonus', 'keywords', fallback='heavy rain warning,heavy snow warning')
-cfg_keywords_nonus  = cfg_kwords_nonus.split(",")
+cfg_keywords_nonus  = cfg_kwords_nonus.lower().split(",")
 
 
 cfg_watch           = cfg.get('alert_conf', 'watch', fallback='off')
@@ -82,11 +81,18 @@ if data_exists:
 
     # COLLECT EACH ALERT FILE
     for post in posts:
-
+        
+        
+        # LOOK FOR KEYWORDS IN TITLE
+        kword_match = "no"
+        for kword in range(len(cfg_keywords_nonus)):
+            if cfg_keywords_nonus[kword].lower() in post.title.lower():
+                kword_match = "yes"
+        
         # LIMIT TO ABOUT A 24 HOUR PERIOD
         dif = (int(round(time.time())) - int(time.strftime('%s', post.published_parsed)))      
-        if dif < 86400:
-
+        if dif < 86400 and post.category == "Met" and kword_match == "yes":
+            
             data_exists = os.path.exists('/tmp/nws_data/nws-' + cfg_feedid_nonus.lower() + '_' + post.guid + '.xml')
             if not data_exists:
                 os.system('wget --quiet -O "/tmp/nws_data/nws-' + cfg_feedid_nonus.lower() + '_' + post.guid + '.xml" ' + post.link + '');
@@ -101,12 +107,13 @@ if data_exists:
                 if post.guid in line:
                     n_seen = "yes"
 
+            
 
 
             # CONTINUE IF ALERT NOT ALREADY SEEN        
             if n_seen == "no":
                 os.system('echo '+post.guid+' >> '+cfg_log+'')
-
+                
                 alert_cap = '/tmp/nws_data/nws-' + cfg_feedid_nonus.lower() + '_' + post.guid + '.xml'
                 alert_src = open(alert_cap, 'r').read()
                 alert_list = CAPParser(alert_src).as_dict()
@@ -114,22 +121,32 @@ if data_exists:
                 alert = alert_list[0]["cap_info"]
 
                 for item in alert:
+                    area_seen           = "no"
                     alert_link          = item["cap_link"]
                     alert_sender        = item["cap_sender_name"]
                     alert_headline      = item["cap_headline"]
                     alert_description   = item["cap_description"]
                     alert_severity      = item["cap_severity"]
-
+                    
+                    
                     for area in item["cap_area"]:
-                        alert_area      = area["area_description"]
+                        if len(area["area_description"]) < 2:
+                            alert_areas      = str(area["area_description"]).lower()
+                            alert_area       = alert_areas
+                        else:
+                            alert_areas      = "multiple"
+                        
+                        for locale in cfg_locales_nonus:
+                            if locale in alert_areas:
+                                area_seen = "yes"
+                            
+                            
+                if alert_severity != "Minor" and area_seen == "yes":
+                    print(f"Headline: {alert_headline} Summary: {alert_description} Area: {alert_area} Link: {alert_link} Sender: {alert_sender}")
 
-                if alert_severity != "Minor":
-                    if alert_area in cfg_locales_nonus:
-                        print(f"Headline: {alert_headline} Summary: {alert_description} Area: {alert_area} Link: {alert_link} Sender: {alert_sender}")
-
-                        cfg_script = f'{cfg_cmd} --urgency=normal --category=im.received --icon=dialog-warning-symbolic "{cfg_region_nonus}: {alert_headline} - {alert_severity}" "{alert_description} Areas: {alert_area} - {alert_sender} - {alert_link}"'
+                    cfg_script = f'{cfg_cmd} --urgency=normal --category=im.received --icon=dialog-warning-symbolic "{cfg_region_nonus}: {alert_headline} - {alert_severity}" "{alert_description} Areas: {alert_area} - {alert_sender} - {alert_link}"'
 
 
-                        #os.system(cfg_script)
+                    os.system(cfg_script)
 else:
     print(f"Main RSS Feed not found for {cfg_feedid_nonus.lower()}")
