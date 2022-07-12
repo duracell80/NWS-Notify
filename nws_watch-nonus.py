d7ad773d42371d9c1ae9c66636655d7f81d71dd0
Lee Jordan <duracell80@users.noreply.github.com>
2022-07-11 21:26:33 -0500

    Adding UK from MetOffice as not reliant on World Met Cap Demo

--- nws_watch-nonus.py
+++ nws_watch-nonus.py
@@ -1,4 +1,4 @@
-import os, subprocess, time, configparser, feedparser, pandas
+import os, re, subprocess, time, configparser, feedparser, pandas
 from datetime import datetime
 
 from capparselib.parsers import CAPParser
@@ -25,6 +25,8 @@ cfg_kwords_nonus    = cfg.get('nws_conf-nonus', 'keywords', fallback='heavy rain
 cfg_keywords_nonus  = cfg_kwords_nonus.lower().split(",")
 
 
+
+
 cfg_watch           = cfg.get('alert_conf', 'watch', fallback='off')
 cfg_voice           = cfg.get('alert_conf', 'voice', fallback='off')
 cfg_alert           = cfg.get('alert_conf', 'alert', fallback='off')
@@ -43,11 +45,14 @@ cfg_para  = ""
 
 
         
-# Main script
+# Main script - WMO
 data_exists = os.path.exists('/tmp/nws_data/nws_data-' + cfg_feedid_nonus.lower() + '.xml')
 if data_exists:
+    os.system('touch /tmp/nws-nonus_seen.txt')
     feed_age = subprocess.check_output('find /tmp/nws_data/nws_data-' + cfg_feedid_nonus.lower() + '.xml -mmin +' + cfg_timer, shell=True, universal_newlines=True)
 else:
+    os.system('touch /tmp/nws-nonus_seen.txt')
+    os.system('mkdir -p /tmp/nws_data')
     os.system('touch /tmp/nws_data/nws_data-' + cfg_feedid_nonus.lower() + '.xml')
     feed_age = "30"
 
@@ -150,3 +155,103 @@ if data_exists:
                     os.system(cfg_script)
 else:
     print(f"Main RSS Feed not found for {cfg_feedid_nonus.lower()}")
+    
+    
+    
+    
+# Main script - UK MET OFFICE
+cfg_urlsht_uk    = cfg.get('nws_conf-uk', 'urlsht', fallback='off')
+cfg_qrcodes_uk   = cfg.get('nws_conf-uk', 'qrcode', fallback='off')
+
+cfg_region_uk    = cfg.get('nws_conf-uk', 'region', fallback='UK')
+cfg_locale_uk    = cfg.get('nws_conf-uk', 'locale', fallback='West Midlands,East Midlands')
+cfg_locales_uk   = cfg_locale_uk.lower().split(",")
+cfg_feedid_uk    = cfg.get('nws_conf-uk', 'feedid', fallback='https://www.metoffice.gov.uk/public/data/PWSCache/WarningsRSS/Region/UK')
+feed_uk          = cfg_feedid_uk
+cfg_kwords_uk    = cfg.get('nws_conf-uk', 'keywords', fallback='extreme heat,flood,thunderstorm warning')
+cfg_keywords_uk  = cfg_kwords_uk.lower().split(",")
+
+
+data_exists = os.path.exists('/tmp/nws_data/nws_data-ukmetoffice.xml')
+if data_exists:
+    os.system('touch /tmp/nws-nonus_seen.txt')
+    feed_age = subprocess.check_output('find /tmp/nws_data/nws_data-ukmetoffice.xml -mmin +' + cfg_timer, shell=True, universal_newlines=True)
+else:
+    os.system('touch /tmp/nws-nonus_seen.txt')
+    os.system('mkdir -p /tmp/nws_data')
+    os.system('touch /tmp/nws_data/nws_data-ukmetoffice.xml')
+    feed_age = "30"
+
+
+if len(feed_age) > 0:
+    os.system('wget --quiet -O "/tmp/nws_data/nws_data-ukmetoffice.xml" ' + feed_uk + '');
+    print("\n\n[-] Weather fetched [ UK MET OFFICE ]")
+else:
+    feed_now = float(time.time())
+    feed_dif = float(feed_now + (float(cfg_timer) * 60))
+    feed_nxt = datetime.fromtimestamp(feed_dif)
+    print("[-] Weather threat monitoring (" + cfg_locale_uk + ", " + cfg_region_uk + " - Next update: " + feed_nxt.strftime('%H:%M:%S') + ")")
+
+if data_exists:
+    feed_xml = open('/tmp/nws_data/nws_data-ukmetoffice.xml', 'r')
+    feed_dat = feed_xml.read()
+    feed_xml.close()
+    alert_feed = feedparser.parse(feed_dat)
+
+    
+    
+    
+
+    # READ THE FEED
+    posts = alert_feed.entries
+
+    #for x in range(len(posts)):
+        #print(posts[x])
+
+    #exit()
+
+    # COLLECT EACH ALERT FILE
+    for post in posts:
+        
+        area_seen = "no"
+        for locale in cfg_locales_uk:
+            if locale in post.description.lower():
+                area_seen = "yes"
+        
+        if area_seen == "yes":
+            url = post.link
+            # SHORTEN URL FIRST ...
+            # To create simpler QR Codes and shorter links in the notification text, change to off in config if tinyURL erroring
+            if cfg_urlsht_uk == "on":
+                import pyshorteners
+
+                shorten = pyshorteners.Shortener()
+                url_sht = shorten.tinyurl.short(url)
+                url = url_sht
+
+            # GENERATE A QRCODE TO THE NWS ... 
+            # Config on or off relates to showing QR in notifcation, so we get QR codes in the /tmp directory to serve
+            import pyqrcode
+            import png
+            from pyqrcode import QRCode
+
+            url_bits = re.findall(r"([A-Za-z0-9\-]+)", post.guid)
+
+            frl = "/tmp/qrcodes/nwsqr_" + url_bits[11] + ".png"
+            qrl = pyqrcode.create(url)
+            qrl.png(frl, scale =10, module_color=[0, 0, 0, 255], background=[0xff, 0xff, 0xff])
+
+            if cfg_qrcodes_uk == "on":
+                if cfg_urlsht_uk == "on":
+                    cfg_script = f'{cfg_cmd} --hint=string:image-path:{frl} --urgency=normal --category=im.received --icon=dialog-warning-symbolic "{cfg_region_uk}: {post.title}" "{post.description} - UK Met Office - {url}"'
+                else:
+                    cfg_script = f'{cfg_cmd} --hint=string:image-path:{frl} --urgency=normal --category=im.received --icon=dialog-warning-symbolic "{cfg_region_uk}: {post.title}" "{post.description} - UK Met Office"'
+            else:
+                cfg_script = f'{cfg_cmd} --urgency=normal --category=im.received --icon=dialog-warning-symbolic "{cfg_region_uk}: {post.title}" "{post.description} - UK Met Office - {url}"'
+
+            os.system(cfg_script)
+        
+        
+        
+else:
+    print(f"Main RSS Feed not found for {feed_uk}")
